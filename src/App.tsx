6754abcd12e8753f48cc7ce357530c14bd72f03f
marlopc <lucaspanaro1@gmail.com>
2022-01-23 17:18:26 -0300

    improves performance of nav and drawer, fix an style error in search component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import PeopleStorage from './contexts/PeopleStorage';
 import Drawer from './components/Drawer';
-import useDarkMode from './hooks/useDarkMode';
+import useDarkMode, { Theme } from './hooks/useDarkMode';
 import useMenu from './hooks/useMenu';
 import Home from './pages/Home';
 import Add from './pages/Add';
@@ -23,15 +23,24 @@ export interface Person {
   timestamp: number,
 }
 
+const Navigation: React.FC<{theme: Theme}> = ({ theme }): React.ReactElement => {
+  const drawer = useMenu();
+  
+  return (
+    <>
+      <Nav drawer={drawer} />
+      <Drawer theme={theme} isOpen={drawer.isOpen} close={drawer.closeMenu}/>
+    </>
+  )
+};
+
 const App = (): React.ReactElement => {
   const theme = useDarkMode();
-  const drawer = useMenu();
-
+  
   return (
     <div className='App' data-theme={theme.current}>
+      <Navigation theme={theme}/>
       <PeopleStorage>
-        <Nav drawer={drawer} />
-        <Drawer theme={theme} isOpen={drawer.isOpen} close={drawer.closeMenu}/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/add' element={<Add />} />
