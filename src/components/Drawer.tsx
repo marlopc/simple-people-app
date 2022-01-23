c90f4c3d7130e74710a3c6e5428cb092e126c67e
marlopc <lucaspanaro1@gmail.com>
2022-01-23 00:03:34 -0300

    removes unused variable

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { Theme } from "../hooks/useDarkMode";
 import { MenuControls } from "../hooks/useMenu";
 import '../styles/Drawer.css';
